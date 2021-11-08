b5bc14641716d86afbec5e67852453efc9948c40
Julaxe <julaxe8@gmail.com>
2021-11-08 16:12:03 -0500

    GameRooms list created and tested

--- SimplestServer-main/SimplestServer/Assets/[Scripts]/NetworkedServer.cs
+++ SimplestServer-main/SimplestServer/Assets/[Scripts]/NetworkedServer.cs
@@ -16,11 +16,13 @@ public class NetworkedServer : MonoBehaviour
     List<int> connectedUsers;
     List<ConnectedAccount> loggedUsers;
     List<ConnectedAccount> inQueueUsers;
+    List<GameRoom> _gameRooms;
     private GameObject templateUIUser;
     private Transform transformRegisteredUsers;
     private Transform transfromLoggedUsers;
     private Transform transfromConnectedUsers;
     private Transform transfromInQueueUsers;
+    private Transform transfromGameRooms;
 
     void Start()
     {
@@ -38,12 +40,14 @@ public class NetworkedServer : MonoBehaviour
         transfromLoggedUsers = GameObject.Find("Canvas/UsersLoggedIn/Scroll").transform;
         transfromConnectedUsers = GameObject.Find("Canvas/UsersConnected/Scroll").transform;
         transfromInQueueUsers = GameObject.Find("Canvas/UsersInQueue/Scroll").transform;
+        transfromGameRooms = GameObject.Find("Canvas/GameRooms/Scroll").transform;
         templateUIUser = Resources.Load<GameObject>("Prefabs/User");
 
         userAccounts = new List<UserAccount>();
         connectedUsers = new List<int>();
         loggedUsers = new List<ConnectedAccount>();
         inQueueUsers = new List<ConnectedAccount>();
+        _gameRooms = new List<GameRoom>();
         LoadUserAccounts();
 
     }
@@ -155,7 +159,11 @@ public class NetworkedServer : MonoBehaviour
             if(inQueueUsers.Count > 1)
             {
                 //start match between the 2 first users
-                SendMessageToClient(ServerStatus.Success + "," + ServerClientSignifiers.FindMatch + ",Match Found", id);
+                foreach(var user in inQueueUsers)
+                {
+                    SendMessageToClient(ServerStatus.Success + "," + ServerClientSignifiers.FindMatch + ",Match Found", user.GetConnectionId());
+                }
+                AddGameRoom(inQueueUsers[0], inQueueUsers[1]);
             }
             else
             {
@@ -240,6 +248,22 @@ public class NetworkedServer : MonoBehaviour
         inQueueUsers.Remove(temp);
     }
 
+    private void AddGameRoom(ConnectedAccount p1, ConnectedAccount p2)
+    {
+        GameObject temp = Instantiate(templateUIUser, transfromGameRooms);
+        temp.GetComponent<TMPro.TextMeshProUGUI>().text = p1.GetUser().GetName() + " vs " + p2.GetUser().GetName();
+        _gameRooms.Add(new GameRoom(temp, p1, p2));
+        DeleteInQueueUser(p1.GetConnectionId());
+        DeleteInQueueUser(p2.GetConnectionId());
+    }
+
+    private void DeleteGameRoom(ConnectedAccount any)
+    {
+        GameRoom temp = _gameRooms.Find(x => (x.Player1() == any || x.Player2() == any));
+        Destroy(temp.GetObject());
+        _gameRooms.Remove(temp);
+    }
+
 }
 
 public class UserAccount
@@ -272,6 +296,23 @@ public class ConnectedAccount
     public int GetConnectionId() { return connectionId; }
     public UserAccount GetUser() { return user; }
     public GameObject GetObject() { return obj; }
+}
+
+public class GameRoom
+{
+    public GameRoom(GameObject obj, ConnectedAccount account1, ConnectedAccount account2)
+    {
+        _obj = obj;
+        _player1 = account1;
+        _player2 = account2;
+    }
+    private ConnectedAccount _player1;
+    private ConnectedAccount _player2;
+    private GameObject _obj;
+
+    public GameObject GetObject() { return _obj; }
+    public ConnectedAccount Player1() { return _player1; }
+    public ConnectedAccount Player2() { return _player2; }
 }
 
 public static class ServerClientSignifiers
